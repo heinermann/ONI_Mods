76a95152a10c645e45798fac3651ace105e15594
Heinermann <aheinerm@gmail.com>
2019-05-19 02:11:17 -0700

    Added DisableSplashScreen mod

---
+++ DisableSplashScreen/Patches/SplashMessageScreen.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+
+namespace Heinermann.DisableSplashScreen.Patches
+{
+  [HarmonyPatch(typeof(SplashMessageScreen), "OnSpawn")]
+  class SplashMessageScreen_OnSpawn
+  {
+    static bool Prefix(ref SplashMessageScreen __instance)
+    {
+      __instance.gameObject.SetActive(false);
+      return false;
+    }
+  }
+}
---
+++ DisableSplashScreen/README.md
@@ -0,0 +1,3 @@
+## DisableSplashScreen
+
+Disables the "Welcome to Oxygen Not Included!" splash screen on startup.
